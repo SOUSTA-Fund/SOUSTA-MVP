938265cc8336a3bd41ea340f97402c9f3c3907bb
Paul Garcia <gp22@users.noreply.github.com>
2023-07-04 12:38:07 -0600

    Add components

--- frontend/src/components/Mint.js
+++ frontend/src/components/Mint.js
@@ -1,1 +1,48 @@
 import React from 'react'
+
+export function Mint({ deployToken, tokenSymbol }) {
+  return (
+    <div>
+      <h4>Mint SOUSTA Token</h4>
+      <form
+        onSubmit={(event) => {
+          // This function just calls the deployToken callback with the
+          // form's data.
+          event.preventDefault()
+
+          const formData = new FormData(event.target)
+          const name = formData.get('name')
+          const ticker = formData.get('ticker')
+          const supply = formData.get('supply')
+
+          if (name && ticker && supply) {
+            deployToken(name, ticker, supply)
+          }
+        }}
+      >
+        <div className="form-group">
+          <label>Token Name</label>
+          <input className="form-control" type="text" name="name" required />
+        </div>
+        <div className="form-group">
+          <label>Token Ticker</label>
+          <input className="form-control" type="text" name="ticker" required />
+        </div>
+        <div className="form-group">
+          <label>Initial Supply of {tokenSymbol}</label>
+          <input
+            className="form-control"
+            type="number"
+            step="1"
+            name="supply"
+            placeholder="1"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input className="btn btn-primary" type="submit" value="Mint" />
+        </div>
+      </form>
+    </div>
+  )
+}
---
+++ frontend/src/components/TokenData.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export function TokenData({ numTokens }) {
+  return (
+    <div>
+      <h4>SOUSTA Token Data</h4>
+      <p>Total Number of Tokens: {numTokens}</p>
+    </div>
+  )
+}
