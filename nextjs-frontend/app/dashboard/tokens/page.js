9f1a305bfd99cdcbf13ebc75dabaf84584a6f7da
Paul Garcia <gp22@users.noreply.github.com>
2023-07-08 21:15:35 -0600

    Remove fragment

--- nextjs-frontend/app/dashboard/tokens/page.js
+++ nextjs-frontend/app/dashboard/tokens/page.js
@@ -64,54 +64,52 @@ export default function Tokens() {
         {!!addresses.length && (
           <ul className="grid grid-cols-3 gap-4">
             {addresses.map((address) => (
-              <>
-                <li
-                  className="p-4 bg-white shadow-lg rounded-2xl dark:bg-gray-700"
-                  key={address}
-                >
-                  <div className="flex font-bold items-center mb-4 text-xl">
-                    <FontAwesomeIcon
-                      style={{ height: 44, width: 44 }}
-                      icon={faBitcoin}
-                    />
-                    <div className="flex flex-col items-start ml-4">
-                      <span>{tokenData.tokens[address]['name']}</span>
-                      <span className="px-2 py-1 font-semibold text-blue-500 bg-blue-100 rounded-md text-xs">
-                        {tokenData.tokens[address]['symbol']}
-                      </span>
-                    </div>
+              <li
+                className="p-4 bg-white shadow-lg rounded-2xl dark:bg-gray-700"
+                key={address}
+              >
+                <div className="flex font-bold items-center mb-4 text-xl">
+                  <FontAwesomeIcon
+                    style={{ height: 44, width: 44 }}
+                    icon={faBitcoin}
+                  />
+                  <div className="flex flex-col items-start ml-4">
+                    <span>{tokenData.tokens[address]['name']}</span>
+                    <span className="px-2 py-1 font-semibold text-blue-500 bg-blue-100 rounded-md text-xs">
+                      {tokenData.tokens[address]['symbol']}
+                    </span>
                   </div>
-                  <div className="border-b-2 border-gray-100 mb-2 pb-2">
-                    <span className="text-gray-500 mr-2 text-sm">
-                      Total Supply
-                    </span>{' '}
-                    {tokenData.tokens[address]['totalSupply']}
-                  </div>
-                  <div className="border-b-2 border-gray-100 mb-2 pb-2 text-sm">
-                    <span className="text-gray-500 mr-2">Contract Address</span>
-                    <br />
-                    <p className="font-mono truncate">{address}</p>
-                  </div>
-                  <div className="mb-4">
-                    <a
-                      href={`https://explorer.testnet.rsk.co/address/${address}`}
-                      className="underline"
-                      target="_blank"
-                    >
-                      View {tokenData.tokens[address]['symbol']} on Rootstock
-                      Explorer
-                    </a>
-                  </div>
-                  <div>
-                    <a
-                      href=""
-                      className="bg-zinc-800 inline-block px-6 py-2 rounded-md text-white"
-                    >
-                      Interact With {tokenData.tokens[address]['symbol']}
-                    </a>
-                  </div>
-                </li>
-              </>
+                </div>
+                <div className="border-b-2 border-gray-100 mb-2 pb-2">
+                  <span className="text-gray-500 mr-2 text-sm">
+                    Total Supply
+                  </span>{' '}
+                  {tokenData.tokens[address]['totalSupply']}
+                </div>
+                <div className="border-b-2 border-gray-100 mb-2 pb-2 text-sm">
+                  <span className="text-gray-500 mr-2">Contract Address</span>
+                  <br />
+                  <p className="font-mono truncate">{address}</p>
+                </div>
+                <div className="mb-4">
+                  <a
+                    href={`https://explorer.testnet.rsk.co/address/${address}`}
+                    className="underline"
+                    target="_blank"
+                  >
+                    View {tokenData.tokens[address]['symbol']} on Rootstock
+                    Explorer
+                  </a>
+                </div>
+                <div>
+                  <a
+                    href=""
+                    className="bg-zinc-800 inline-block px-6 py-2 rounded-md text-white"
+                  >
+                    Interact With {tokenData.tokens[address]['symbol']}
+                  </a>
+                </div>
+              </li>
             ))}
           </ul>
         )}
