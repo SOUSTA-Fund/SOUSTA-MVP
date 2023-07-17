c7f95011ad038ff7b328f9e16f46853d3dab764a
Paul Garcia <gp22@users.noreply.github.com>
2023-07-17 17:12:02 -0600

    Remove section

--- frontend/app/(landingpage)/page.js
+++ frontend/app/(landingpage)/page.js
@@ -75,8 +75,8 @@ export default function Home() {
 
   return (
     <>
-      <main className="flex flex-col items-center justify-between py-24">
-        <div className="grid grid-cols-2 relative pb-24 px-24">
+      <main className="flex flex-col items-center justify-between">
+        <div className="grid grid-cols-2 relative p-24">
           <div>
             <h1 className="font-bold leading-tight mb-4 text-5xl text-blue-900">
               Bridging the gap between real-world assets and blockchain
@@ -191,59 +191,6 @@ export default function Home() {
             })}
           </ul>
         </section>
-
-        <div className="mb-32 grid text-center px-24 lg:mb-0 lg:grid-cols-4 lg:text-left">
-          <a
-            href=""
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              FAQ{' '}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Get answers to frequently asked questions.
-            </p>
-          </a>
-
-          <a
-            href=""
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              Docs{' '}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Learn how SOUSTA works.
-            </p>
-          </a>
-
-          <a
-            href=""
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              Governance{' '}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Learn about how SOUSTA is governed.
-            </p>
-          </a>
-        </div>
       </main>
     </>
   )
